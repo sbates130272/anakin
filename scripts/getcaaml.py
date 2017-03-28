f4377f435c6575e803b46233cafb4427d4025e9b
Stephen Bates <sbates@raithlin.com>
2017-03-28 21:48:10 +0000

    getcamml.py: Error handling and file saving options
    Add some error handling for when CAAML records are not there. Also
    added some file options.

--- scripts/getcaaml.py
+++ scripts/getcaaml.py
@@ -10,6 +10,7 @@
 # Note that right now we just get the data and put each day into a
 # file. We will process this data down the road.
 
+import os
 import argparse
 import urllib2
 import time
@@ -21,16 +22,23 @@ CAAML_DATE_FMT = "%Y-%m-%d"
 def getdays(args):
     start = datetime.strptime(args.start, CAAML_DATE_FMT)
     for date in (start - timedelta(x) for x in range(args.days)):
-        __getday(args, date.strftime(CAAML_DATE_FMT))
+        day = __getday(args, date.strftime(CAAML_DATE_FMT))
+        if day:
+            __writeday(args, day, date.strftime(CAAML_DATE_FMT))
 
 def __getday(args, date):
-    resp = urllib2.urlopen('%s?d=%s&r=%d' \
-                    % (args.www, date, args.region))
-    print resp
-    data = resp.read()
-    resp.close()
+    try:
+        resp = urllib2.urlopen('%s?d=%s&r=%d' \
+                               % (args.www, date, args.region))
+    except IOError:
+        return None
 
-    print data
+    return resp
+
+def __writeday(args, day, date):
+    file = open(os.path.join(args.odir, date+".xml"),'w')
+    file.write(day.read())
+    file.close()
 
 if __name__=="__main__":
 
@@ -44,6 +52,10 @@ if __name__=="__main__":
                         help='Start date for CAAML data (work backwards from here)')
     parser.add_argument('-r', '--region', type=int, default=1,
                         help='The region to pull the data from')
+    parser.add_argument('-l', '--log', default=None,
+                        help='Logfile to record information')
+    parser.add_argument('-o', '--odir', default='.',
+                        help='Folder for the output data')
     parser.add_argument('-w', '--www',
                         default='http://avalanche.pc.gc.ca/CAAML-eng.aspx',
                         help='The region to pull the data from')
